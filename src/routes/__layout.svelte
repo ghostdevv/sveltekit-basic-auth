e9ba9eba53b6f1d3091a6b6e48c83874c3eb83ce
GHOSt <ghostdevbusiness@gmail.com>
2022-04-01 15:57:36 +0100

    refactor: move to layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script context="module" lang="ts">
+    import type { Load } from '@sveltejs/kit';
+    import { user } from '$lib/user';
+
+    export const load: Load = ({ session }) => {
+        user.set(session.user);
+
+        return {
+            status: 200,
+        };
+    };
+</script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,3 @@
-<script context="module" lang="ts">
-    import type { Load } from '@sveltejs/kit';
-
-    export const load: Load = ({ session }) => {
-        user.set(session.user);
-
-        return {
-            status: 200,
-        };
-    };
-</script>
-
 <script>
     import { user } from '$lib/user';
 </script>
